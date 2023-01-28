185b1660193d9b70fbb96d8433261fadb60926d1
CF <cfreitas12345@gmail.com>
2023-01-28 01:40:10 -0700

    Redid plans for JS.

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// - Acceptance Criteria
+// I made a weather dashboard that would run in the browser and feature dynamically updated HTML and CSS.  
+// It shows the weather outlook for multiple cities, so anyone using it could plan a trip accordingly.
+
+// 1) It has a weather dashoard with form inputs.
+// 2) When searching for a city, there's current and future conditions.
+// 3) That city is added to the search history.
+// 4) When viewing current weather contions for that city it has the city name, date, 
+//    and icon representation of weather conditions, temperature, humidity, and wind speed.
+// 5) Future weather conditions show: A 5-day forecast that displays the date, 
+//    an icon representation of weather conditions, the temp., wind speed, & humidty.
+// 6) When clicking onto a city in the search history, there's current and future conditions.
+
+// var
+
+// function
+
+// local storage
